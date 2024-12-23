9e99f422a2ca492cae1bb529a95d2b1e9c24a618
yash <yashvikram8250@gmail.com>
2024-12-23 15:25:31 +0530

    Added contentSchema and model

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,16 @@ import mongoose, { Schema, Document } from "mongoose";
 
 // Create an interface for the User
 interface IUser extends Document {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
+}
+
+interface IContent extends Document{
+    link: string,
+    type: string,
+    title: string,
+    tags: mongoose.Types.ObjectId,
+    userId: mongoose.Types.ObjectId,
 }
 
 const userSchema = new Schema({
@@ -13,3 +21,13 @@ const userSchema = new Schema({
 
 // Export the model directly instead of in an object
 export const UserModel = mongoose.model<IUser>('users', userSchema);
+
+const contentSchema = new Schema({
+    link: {type: String , required: true},
+    type: {type: String, required:true},
+    title: {type: String, required: true },
+    tags: [{ type: mongoose.Types.ObjectId, ref: 'Tag' }],
+    userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+})
+
+export const contentModel = mongoose.model<IContent>('contents',contentSchema);
