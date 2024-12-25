39ddbe8f48469138dc948237a66a96b478150e4e
yash <yashvikram8250@gmail.com>
2024-12-25 10:04:15 +0530

    Building the button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import Button from "./components/ui/Button"
+import {Plus} from "./icons/Plus"
+import Share from "./icons/Share"
 
 function App() {
-
-
   return (
    <>
+    <Button variant="primary" text="Share" size="sm" startIcon={<Plus size="sm"/>} endIcon={<Share size="sm"/>}/>
+    <Button variant="secondary" text="Share" size="md" startIcon={<Plus size="md"/>} endIcon={<Share size="md"/>}/>
+    <Button variant="secondary" text="Share" size="lg" startIcon={<Plus size="lg"/>} endIcon={<Share size="lg"/>}/>
    </>
   )
 }
--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -6,13 +6,33 @@ export interface Buttonprops{
     text: string;
     startIcon?: ReactNode;
     endIcon?: ReactNode;
-    onClick:()=>void ;
+    onClick?:()=>void ;
 }
 
-export default function Button() {
+const variantStyles = {
+    "primary" : "bg-purple-600 text-white",
+    "secondary" : "bg-purple-300 text-purple-600"
+}
+
+const sizeStyles = {
+  "sm" : "px-2 py-1 text-sm rounded-sm",
+  "md" : "px-4 py-2 text-md rounded-md",
+  "lg" : "px-8 py-4 text-xl rounded-xl",
+}
+
+const defaultstyles = "rounded-md flex items-center w-auto";
+
+export default function Button(props:Buttonprops){
+
   return (
     <div>
-      
+      <button className={`${variantStyles[props.variant]} ${defaultstyles} ${sizeStyles[props.size]}`} >
+        {props.startIcon }
+        <div className="px-2">
+          {props.text} 
+        </div>
+        {props.endIcon}
+        </button>
     </div>
   )
 }
