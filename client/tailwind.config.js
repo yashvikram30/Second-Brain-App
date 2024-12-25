68c1ad60343d454151835e1b6ea6ab19560fd06a
yash <yashvikram8250@gmail.com>
2024-12-25 10:03:22 +0530

    adding colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          300: "#e0e7fe",
+          500: "3e38a7",
+          600: "#5046e4",
+        }
+      }
+    },
   },
   plugins: [],
 }
