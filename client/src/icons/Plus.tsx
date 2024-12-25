1cf9e1e6850ec5a7df27e8a12646d40e68fb139b
yash <yashvikram8250@gmail.com>
2024-12-25 10:03:41 +0530

    Adding icon library

---
+++ client/src/icons/Plus.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from ".";
+import { iconSizeVariant } from ".";
+
+export const Plus = (props : IconProps)  => {
+  return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={iconSizeVariant[props.size]}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+        />
+      </svg>
+  );
+};
---
+++ client/src/icons/index.ts
@@ -0,0 +1,9 @@
+export interface IconProps{
+    size: "sm" | "md" | "lg"
+}
+
+export const iconSizeVariant = {
+    "sm": "size-4",
+    "md": "size-5",
+    "lg": "size-6"
+}
