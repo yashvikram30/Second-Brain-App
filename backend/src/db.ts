f9e6e4da070dcfd54ee561d1f69846a07d2dc7e1
yash <yashvikram8250@gmail.com>
2024-12-26 09:05:43 +0530

    Added linkSchema and model

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,18 +1,18 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 // Create an interface for the User
-interface IUser extends Document {
-  username: string;
-  password: string;
-}
+// interface IUser extends Document {
+//   username: string;
+//   password: string;
+// }
 
-interface IContent extends Document {
-  link: string;
-  type: string;
-  title: string;
-  tags: mongoose.Types.ObjectId[];
-  userId: mongoose.Types.ObjectId;
-}
+// interface IContent extends Document {
+//   link: string;
+//   type: string;
+//   title: string;
+//   tags: mongoose.Types.ObjectId[];
+//   userId: mongoose.Types.ObjectId;
+// }
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
@@ -20,7 +20,7 @@ const userSchema = new Schema({
 });
 
 // Export with consistent model name
-export const User = mongoose.model<IUser>('User', userSchema);
+export const User = mongoose.model('User', userSchema);
 
 const contentSchema = new Schema({
   link: { type: String, required: true },
@@ -30,4 +30,11 @@ const contentSchema = new Schema({
   userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true }, // The ref name should match the model name, else the program will throw an error
 });
 
-export const Content = mongoose.model<IContent>('Content', contentSchema);
+export const Content = mongoose.model('Content', contentSchema);
+
+const linkSchema = new mongoose.Schema({
+  hash: String,
+  userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true, unique:true },
+})
+
+export const Link = mongoose.model('Link',linkSchema);
